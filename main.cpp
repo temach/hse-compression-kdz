cad68ea7e0c3cbcab68b995320c9b01a12dbfb48
Artem Abramov <tematibr@gmail.com>
2016-11-23 00:46:08 +0300

    Huffman encode/decode works with string streams.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <memory>
 #include <fstream>
+#include <sstream>
 #include <map>
 #include <stdexcept>
 #include <utility>
@@ -12,33 +13,9 @@
 
 using namespace std;
 
-class EncodeShennon {
-public:
-
-
-};
-
-class DecodeShennon {
-public:
-
-
-};
-
-class EncodeHuffman {
-public:
-
-
-};
-
-class DecodeHuffman {
-public:
-
-
-};
-
-class ArgParser {
+class ArgParser
+{
     public:
-
         ArgParser  (int &argc, char **argv){
             for (int i=1; i < argc; ++i) {
                 this->tokens.push_back(string(argv[i]));
@@ -63,16 +40,19 @@ class ArgParser {
         std::vector <std::string> tokens;
 };
 
-enum class ALGORITHM : bool {
+enum class ALGORITHM : bool
+{
     huffman = true,
     shennon = false
 };
 
-
 typedef std::vector<bool> HuffCode;
-typedef std::map<char, HuffCode> HuffCodeMap;
-typedef std::map<HuffCode, char> Code2LetterHuffmanMap;
+typedef std::map<char, HuffCode> EncodeHuffmanMap;
+typedef std::map<HuffCode, char> DecodeHuffmanMap;
 typedef std::map<char,int> FrequencyTable;
+
+class INode;
+typedef std::shared_ptr<INode> NodePtr;
 
 class INode
 {
@@ -86,7 +66,6 @@ protected:
     INode(int f) : f(f) {}
 };
 
-typedef std::shared_ptr<INode> NodePtr;
 
 class InternalNode : public INode
 {
@@ -110,159 +89,198 @@ struct NodeCmp
     bool operator()(const NodePtr lhs, const NodePtr rhs) const { return lhs->f > rhs->f; }
 };
 
-NodePtr BuildTree(const FrequencyTable& freq_table)
+
+class IEncoder
 {
-    std::priority_queue<NodePtr, std::vector<NodePtr>, NodeCmp> trees;
-
-    for (const auto& stats : freq_table)
-    {
-        NodePtr np(new LeafNode(stats.second, stats.first));
-        trees.push(np);
-    }
-    while (trees.size() > 1)
-    {
-        NodePtr childR = trees.top();
-        trees.pop();
-        NodePtr childL = trees.top();
-        trees.pop();
-        NodePtr parent(new InternalNode(childR, childL));
-        trees.push(parent);
-    }
-    return trees.top();
-}
-
-void GenerateCodes(const NodePtr node, const HuffCode& prefix, HuffCodeMap& outCodes)
-{
-    if (const shared_ptr<const LeafNode> lf = dynamic_pointer_cast<const LeafNode>(node))
-    {
-        outCodes[lf->c] = prefix;
-    }
-    else if (const shared_ptr<const InternalNode> in = dynamic_pointer_cast<const InternalNode>(node))
-    {
-        HuffCode leftPrefix = prefix;
-        leftPrefix.push_back(false);
-        GenerateCodes(in->left, leftPrefix, outCodes);
-        HuffCode rightPrefix = prefix;
-        rightPrefix.push_back(true);
-        GenerateCodes(in->right, rightPrefix, outCodes);
-    }
-}
-
-void WriteHuffmanTree(const NodePtr node, string& out)
-{
-    if (const shared_ptr<const LeafNode> lf = dynamic_pointer_cast<const LeafNode>(node))
-    {
-        out += "1-";
-        out += lf->c;
-        out += "-";
-    }
-    else if (const shared_ptr<const InternalNode> in = dynamic_pointer_cast<const InternalNode>(node))
-    {
-        out += '0';
-        WriteHuffmanTree(in->left, out);
-        WriteHuffmanTree(in->right, out);
-    }
-}
-
-void ReadHuffmanTree(const HuffCode& prefix,  Code2LetterHuffmanMap& outCodes, ifstream& input)
-{
-    char now;
-    if (! input.get(now)) {
-        throw runtime_error("Could not reconstruct tree.");
-    }
-    if (now == '1')
-    {
-        // read letter
-        char data[4];
-        input >> data;
-        char letter = data[1];
-        // add to map
-        outCodes[prefix] = letter;
-    }
-    else if (now == '0')
-    {
-        HuffCode leftPrefix = prefix;
-        leftPrefix.push_back(false);
-        ReadHuffmanTree(leftPrefix, outCodes, input);
-        HuffCode rightPrefix = prefix;
-        rightPrefix.push_back(true);
-        ReadHuffmanTree(rightPrefix, outCodes, input);
-    }
-}
-
-void FillFrequencyTable(FrequencyTable& freq_table, const string& infile) {
-    ifstream in;
-    in.open(infile);
-    if (in.is_open()) {
-
-        // we can continue
-        char ch;
-        while (in.get(ch)) {
-            ++freq_table[ch];
+    public:
+        void FillFrequencyTable(iostream& is) {
+            if (is.good()) {
+                // we can continue
+                char ch;
+                while (is.get(ch)) {
+                    ++(this->table)[ch];
+                }
+                if (is.eof()) {
+                    // clear eof and rewind input stream
+                    is.clear();
+                    is.seekg(0, std::ios::beg);
+                } else {
+                    // we stopped reading the file, but it is not EOF yet.
+                    throw std::runtime_error("Could not read file");
+                }
+                // we reached eof. all ok
+            }
         }
-        if (! in.eof()) {
-            // we stopped reading the file, but it is not EOF yet.
-            throw std::runtime_error("Could not read file");
-        }
-        // we reached eof. all ok
-    }
-    in.close();
-}
 
-void TransformEncode(string infile, string outfile, HuffCodeMap& encodemap) 
-{
-    ifstream in;
-    in.open(infile);
-    ofstream out;
-    out.open(outfile, std::ofstream::app | std::ofstream::out);
-    if (in.is_open() && out.is_open()) {
-        // we can continue
-        char in_ch;
-        while (in.get(in_ch)) 
+        void GenerateCodes()
         {
-            string enc;
-            for (const auto& bit : encodemap[in_ch]) {
-                enc += bit ? "1" : "0";
+            InnerGenerateCodes(root, HuffCode{});
+        }
+
+        void InnerGenerateCodes(const NodePtr node, const HuffCode& prefix)
+        {
+            if (const shared_ptr<const LeafNode> lf = dynamic_pointer_cast<const LeafNode>(node))
+            {
+                this->ch2code[lf->c] = prefix;
             }
-            out << enc;
-        }
-        if (! in.eof()) {
-            // we stopped reading the file, but it is not EOF yet.
-            throw std::runtime_error("Could not encode");
-        }
-        // we reached eof. all ok
-    }
-    in.close();
-    out.close();
-}
-
-void TransformEncode(string infile, string outfile, HuffCodeMap& encodemap) {
-    ifstream in;
-    in.open(infile);
-    ofstream out;
-    out.open(outfile, std::ofstream::app | std::ofstream::out);
-    if (in.is_open() && out.is_open()) {
-        // we can continue
-        char in_ch;
-        while (in.get(in_ch)) {
-            string enc;
-            for (const auto& bit : encodemap[in_ch]) {
-                enc += bit ? "1" : "0";
+            else if (const shared_ptr<const InternalNode> in = dynamic_pointer_cast<const InternalNode>(node))
+            {
+                HuffCode leftPrefix = prefix;
+                leftPrefix.push_back(false);
+                InnerGenerateCodes(in->left, leftPrefix);
+                HuffCode rightPrefix = prefix;
+                rightPrefix.push_back(true);
+                InnerGenerateCodes(in->right, rightPrefix);
             }
-            out << enc;
         }
-        if (! in.eof()) {
-            // we stopped reading the file, but it is not EOF yet.
-            throw std::runtime_error("Could not encode");
+
+        void WriteHuffmanTree(iostream& os)
+        {
+            InnerWriteHuffmanTree(root, os);
         }
-        // we reached eof. all ok
-    }
-    in.close();
-    out.close();
-}
+
+        void InnerWriteHuffmanTree(const NodePtr node, iostream& os)
+        {
+            if (const shared_ptr<const LeafNode> lf = dynamic_pointer_cast<const LeafNode>(node))
+            {
+                os << "1-" << lf->c << "-";
+            }
+            else if (const shared_ptr<const InternalNode> in = dynamic_pointer_cast<const InternalNode>(node))
+            {
+                os << '0';
+                InnerWriteHuffmanTree(in->left, os);
+                InnerWriteHuffmanTree(in->right, os);
+            }
+        }
+
+    shared_ptr<INode> root;
+    FrequencyTable table;
+    EncodeHuffmanMap ch2code;
+};
 
 
-int main(int argc, char **argv){
+class EncodeHuffman : public IEncoder
+{
+    public:
+        void BuildTree()
+        {
+            std::priority_queue<NodePtr, std::vector<NodePtr>, NodeCmp> trees;
+            for (const auto& stats : table)
+            {
+                NodePtr np{new LeafNode{stats.second, stats.first}};
+                trees.push(np);
+            }
+            while (trees.size() > 1)
+            {
+                NodePtr childR = trees.top();
+                trees.pop();
+                NodePtr childL = trees.top();
+                trees.pop();
+                NodePtr parent{new InternalNode{childR, childL}};
+                trees.push(parent);
+            }
+            root = shared_ptr<INode>{trees.top()};
+        }
+
+
+        void TransformEncode(iostream& is, iostream& os)
+        {
+            if (is.good() && os.good()) {
+                // we can continue
+                char in_ch;
+                while (is.get(in_ch))
+                {
+                    string code;
+                    for (const auto& bit : this->ch2code[in_ch]) {
+                        code += bit ? "1" : "0";
+                    }
+                    os << code;
+                }
+                if (is.eof()) {
+                    // clear eof and rewind input stream
+                    is.clear();
+                    is.seekg(0, std::ios::beg);
+                } else {
+                    // we stopped reading the file, but it is not EOF yet.
+                    throw std::runtime_error("Could not encode");
+                }
+                // we reached eof. all ok
+            }
+        }
+
+};
+
+
+class IDecoder
+{
+    public:
+        IDecoder() : code2ch{} { }
+
+        void ReadHuffmanTree(iostream& is)
+        {
+            auto var = HuffCode();
+            InnerReadHuffmanTree(var, is);
+        }
+
+        void InnerReadHuffmanTree(const HuffCode& prefix, iostream& is)
+        {
+            char now;
+            if (! is.get(now)) {
+                throw runtime_error("Could not reconstruct tree.");
+            }
+            if (now == '1')
+            {
+                // read letter
+                char data[3];
+                is.read(data, 3);
+                char letter = data[1];
+                // add to map
+                code2ch[prefix] = letter;
+            }
+            else if (now == '0')
+            {
+                HuffCode leftPrefix = prefix;
+                leftPrefix.push_back(false);
+                InnerReadHuffmanTree(leftPrefix, is);
+                HuffCode rightPrefix = prefix;
+                rightPrefix.push_back(true);
+                InnerReadHuffmanTree(rightPrefix, is);
+            }
+        }
+
+    DecodeHuffmanMap code2ch{};
+};
+
+class DecodeHuffman : public IDecoder
+{
+    public:
+        void TransformDecode(iostream& is, iostream& os)
+        {
+            if (is.good() && os.good()) {
+                // we can continue
+                char in_ch;
+                HuffCode code{};
+                while (is.get(in_ch))
+                {
+                    code.push_back(in_ch=='1' ? true : false);
+                    if(code2ch.find(code) != code2ch.end()) {
+                        os << code2ch[code];
+                        code.clear();
+                    }
+                }
+                if (! is.eof()) {
+                    // we stopped reading the file, but it is not EOF yet.
+                    throw std::runtime_error("Could not encode");
+                }
+                // we reached eof. all ok
+            }
+        }
+
+};
+
+
+int main(int argc, char **argv)
+{
     // Parse agruments
     bool show_help = false;
     // Check that options are valid
@@ -293,53 +311,32 @@ int main(int argc, char **argv){
         return -1;
     }
 
-    // Count the frquencies
-    FrequencyTable freq_table;
-    // FillFrequencyTable(freq_table, infile);
-    freq_table.emplace('1',5);
-    freq_table.emplace('2',7);
-    freq_table.emplace('3',10);
-    freq_table.emplace('4',15);
-    freq_table.emplace('5',20);
-    freq_table.emplace('6',45);
-    NodePtr root = BuildTree(freq_table);
-    HuffCodeMap codes;
-    GenerateCodes(root, HuffCode(), codes);
+    EncodeHuffman enc{};
+    string indata
+      = "1111122222223333333333444444444444444555555555555555555555666666666666666666666666666666666666666666666";
+    stringstream indatastream{indata};
+    enc.FillFrequencyTable(indatastream);
+    enc.BuildTree();
+    enc.GenerateCodes();
 
-    for (HuffCodeMap::const_iterator it = codes.begin(); it != codes.end(); ++it)
+    for (EncodeHuffmanMap::const_iterator it = enc.ch2code.begin(); it != enc.ch2code.end(); ++it)
     {
         std::cout << it->first << " ";
         std::copy(it->second.begin(), it->second.end(), std::ostream_iterator<bool>(std::cout));
         std::cout << std::endl;
     }
 
-    string hufftree;
-    WriteHuffmanTree(root, hufftree);
-    cout << "___" << hufftree << "___" << endl;
+    stringstream outdatastream{};
+    enc.WriteHuffmanTree(outdatastream);
+    enc.TransformEncode(indatastream, outdatastream);
 
-    ofstream os;
-    os.open(outfile);
-    if (os.is_open()) {
-        os << hufftree;
-    }
-    TransformEncode(infile, outfile, codes);
+    DecodeHuffman dec{};
+    dec.ReadHuffmanTree(outdatastream);
+    stringstream decoded{};
+    dec.TransformDecode(outdatastream, decoded);
+    cout << decoded.str() << endl;
 
-
-    queue<char> q;
-    for (const auto ch : hufftree) {
-        q.push(ch);
-    }
-    Code2LetterHuffmanMap read_map;
-    ifstream is;
-    is.open(outfile);
-    if (is.is_open()) {
-        ReadHuffmanTree(HuffCode(), read_map, is);
-        ofstream other;
-        other.open("testing.again");
-        TransformDecode(is, read_map);
-    }
-
-    for (Code2LetterHuffmanMap::const_iterator it = read_map.begin(); it != read_map.end(); ++it)
+    for (DecodeHuffmanMap::const_iterator it = dec.code2ch.begin(); it != dec.code2ch.end(); ++it)
     {
         std::cout << it->second << " ";
         std::copy(it->first.begin(), it->first.end(), std::ostream_iterator<bool>(std::cout));
@@ -350,8 +347,7 @@ int main(int argc, char **argv){
     // and do the work in each case
     if (algo==ALGORITHM::huffman && infile.find(".txt") != string::npos) {
         // encode with huffman, write name.haff
-        EncodeHuffman enc = EncodeHuffman();
-
+        EncodeHuffman enc;
     }
     else if (algo==ALGORITHM::shennon && infile.find(".txt") != string::npos) {
         // encode with shennon, write name.shan
