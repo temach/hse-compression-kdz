c30f74cef67fef2e3135b75a75cc25dbb31c6fc9
Artem Abramov <tematibr@gmail.com>
2016-12-03 10:16:03 +0300

    Finish script for random data generation

--- gen_file.py
+++ gen_file.py
@@ -1,13 +1,37 @@
 import codecs
+import itertools
+import random
 
-a = "a" * 45000
-b = "b" * 13000
-c = "c" * 12000
-d = "d" * 16000
-e = "e" *  9000
-f = "f" *  5000
-fin = a+b+c+d+e+f
+def data_generator(size, chars):
+    return (random.choice(chars) for _ in range(size))
+
+kibibyte = 2**10
+mebibyte = 2**20
+
+sizes = [
+    20*kibibyte
+    , 40*kibibyte
+    , 60*kibibyte
+    # , 80*kibibyte
+    # , 100*kibibyte
+    # , 1*mebibyte
+    # , 2*mebibyte
+    # , 3*mebibyte
+    ]
+
+# the first set also contains the SPACE character
+set1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz"
+set2 = set1 + "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя"
+set3 = set2 + "+-*/=.,;:?!%@#$&~()[]{}<>\"\'"
+sets = [set1, set2, set3]
+
+samples = [0, 1, 2]
 
 
-with codecs.open("test.txt", "w", "utf-8") as f:
-    f.write(fin)
+x = list(itertools.product(sizes, sets, samples))
+for sz, chars, smple in x:
+    fname = "size-{0}_charset-{1}_sample-{2}".format(sizes.index(sz), sets.index(chars), smple)
+    fullname = "./inputs/{}".format(fname)
+    with codecs.open(fullname, "w", "utf-8") as f:
+        f.write(''.join(data_generator(sz, chars)))
+
